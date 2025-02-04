086d090aa4b53c8a1112dbcc90642f148a8d9467
Owen Yoshishige <owenyoshishige@gmail.com>
2025-02-04 18:14:52 -0500

    Time: 0 ms (100.00%) | Memory: 51.6 MB (49.12%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.ts
@@ -0,0 +1,32 @@
+function wordBreak(target: string, words: string[]): string[] {
+    if (!words.length) return [""];
+
+    const result: string[] = [];
+    const path: string[] = [];
+    // const memo: Map<string, string[]> = new Map();
+
+    const dfs = (start_index: number): void => {
+        if (start_index === target.length) {
+            result.push(path.join(" "));
+            return;
+        }
+
+        const startSubString = target.substring(0, start_index);
+        if (start_index > target.length) return;
+
+        for (const word of words) {
+            if (!target.startsWith(word, start_index)) continue;
+
+            path.push(word);
+            dfs(start_index + word.length);
+            path.pop();
+        }
+
+        // memo.add(startSubString);
+        return;
+    }
+
+    dfs(0);
+
+    return result;
+};
