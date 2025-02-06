616f757dab87e3481f687e3e31dcc97aebae65fd
Owen Yoshishige <owenyoshishige@gmail.com>
2025-02-06 15:34:49 -0500

    Time: 0 ms (100.00%) | Memory: 54 MB (17.13%) - LeetSync

--- 78-subsets/subsets.ts
+++ 78-subsets/subsets.ts
@@ -1,37 +1,3 @@
-/*
-Input: nums = [1,2,3]
-Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-
-constraints: all results must be unqiue
-edgecase: if nums is empty, return empty array of array
-
-backtracking
-
-main(nums)
-if nums is empty, return empty array of array
-
-create result array with 1 empty array inside
-create path array
-
-dfs(start_index) {
-    if start_index is equal to nums length
-        return
-    
-    iterate nums
-        push num into path
-        push copy of path into result
-
-        dfs(start_index + 1)
-
-        pop path
-}
-
-dfs(0)
-
-return result
-*/
-
-
 function subsets(nums: number[]): number[][] {
     if (!nums.length) return [[]];
 
