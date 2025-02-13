747c60f0f5f4e22e31d0a00b49178068815799a3
Owen Yoshishige <owenyoshishige@gmail.com>
2025-02-13 12:19:02 -0500

    Time: 13 ms (7.94%) | Memory: 55.2 MB (37.68%) - LeetSync

--- 46-permutations/permutations.ts
+++ 46-permutations/permutations.ts
@@ -1,17 +1,3 @@
-// main(nums)
-// if nums is empty, return empty array
-// if nums has 1 num in it, return array of array with that num
-
-// create result array
-// create collectNums array
-
-
-
-
-// invoke dfs(nums.slice())
-
-//return result
-
 function permute(nums: number[]): number[][] {
     if (!nums.length) return [];
     if (nums.length === 1) return [nums];
@@ -19,22 +5,15 @@ function permute(nums: number[]): number[][] {
     const result = [];
     const collectedNums = [];
 
-    // dfs(remaining: number[]) {}
     const dfs = (remaining: number[]) => {
-        // if collectedNums array is same length as nums array
         if (collectedNums.length === nums.length) {
-            // push copy of collectedNums into result
             result.push(collectedNums.slice(0));
             return;
         }
 
-        // iterate remainingNums
         for (const num of remaining) {
-            // push current number in nums into collectNums array
             collectedNums.push(num);
-            // dfs(remainingNums.filter(currentNumber))
             dfs(remaining.filter((element) => num !== element));
-            // pop the current num from collectNums
             collectedNums.pop();
         }
     };
