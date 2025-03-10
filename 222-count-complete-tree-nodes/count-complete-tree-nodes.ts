096d8349335d9ac9c877353e305ab6baf4ffcd37
Owen Yoshishige <owenyoshishige@gmail.com>
2025-03-10 16:14:16 -0400

    Time: 0 ms (100.00%) | Memory: 75.6 MB (9.05%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.ts
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+
+ /*
+    I - node
+    O - number of nodes
+    C - dfs or bfs
+    E - if no node, return 0
+
+
+    main(root)
+        if no node, return 0
+
+        create result at 1
+
+        result+= main(root.left);
+        result+= main(root.right);
+
+        return result
+
+
+
+ */
+
+function countNodes(root: TreeNode | null): number {
+    if (!root) return 0;
+
+    let result = 1;
+
+    result+= countNodes(root.left);
+    result+= countNodes(root.right);
+
+    return result;
+};
