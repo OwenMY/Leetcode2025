fbd2235e4fa364a643369c8a95ec805d2800fab3
Owen Yoshishige <owenyoshishige@gmail.com>
2025-03-19 12:48:46 -0400

    Time: 0 ms (100.00%) | Memory: 55.9 MB (23.07%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    if (!root) return [];
+
+    const leftResults = inorderTraversal(root.left);
+    const rightResults = inorderTraversal(root.right);
+
+    return [...leftResults, root.val, ...rightResults];
+};
